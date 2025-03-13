20fb5b0a004ead13e6cebd2c4d9f478abf609734
Yogesh Bamanier <bamanieryogesh1@gmail.com>
2025-03-14 00:33:01 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,16 +9,16 @@ const inputStyle = {
 };
 
 const submitButton = {
-    height:'32px',
-    margin:'-3px 10px 0px 2px',
-    backgroundColor: 'white',
+     height:'32px',
+     margin:'-3px 10px 0px 2px',
+     backgroundColor: 'white',
      textAlign: 'center',
-    border: '1px solid blue', 
-    borderRadius: '5px',
-    padding: '7px 10px 5px 10px',
-    cursor: 'pointer',
+     border: '1px solid blue', 
+     borderRadius: '5px',
+     padding: '7px 10px 5px 10px',
+     cursor: 'pointer',
      color: 'blue',
-    fontWeight: '200',
+     fontWeight: '200',
      fontSize: '16px'
 }
 
@@ -32,52 +32,38 @@ class Search extends Component {
             suggest: [],
             isSuggestOpen: false
         }
-
     };
-
-
 
     onSubmit = (e) => {
         e.preventDefault();
         if (this.state.text === '') {
             alert("Please Enter Some Text.");
+            return 
         }
         else {
-
             this.props.searchUsers(this.state.text);
-
             this.setState({ ...this.state, text: '', suggest: [] });
         }
     };
 
     suggestedOnclick = (suggest_login) => {
-    
         this.setState({ text: '', suggest: [] , isSuggestOpen:false });
-
         this.props.searchUsers(suggest_login);
-        console.log(suggest_login);
     }
 
 
 
     suggestFunction = async text => {
-
         await axios.get(`https://api.github.com/search/users?q=${text}`)
             .then( res => {
                 this.setState({ suggest: res.data.items });
                   console.log(res.data)})
-            .catch((error) => console.log('Error in Suggest-function'));
-
-        console.log(this.state.suggest);
-        
+            .catch((error) => console.warn('Error in Suggest-function'));
     }
-
-
-    onChange = e => {
+     onChange = e => {
         this.setState({ [e.target.name]: e.target.value });
         if (e.target.value === '') {
             this.setState({ suggest: [], isSuggestOpen: false });
-
           } 
         else {
             this.setState({ isSuggestOpen: true });
@@ -85,29 +71,20 @@ class Search extends Component {
         }
     }
 
-
-
     render() {
         const { suggest } = this.state;
-
         return (
             <SearchBar>
-
                 <form onSubmit={this.onSubmit} style={inputStyle}>
                     <SearchInput name='text' type='text' placeholder='Search for user... '
                         value={this.state.text} autoComplete='off'
                         onChange={this.onChange} />
-
                     <input type='submit' value='Search' style={submitButton} />
                 </form>
-
-
                 {
                     this.state.isSuggestOpen && 
-                    <AutoSuggestion suggests={suggest} searchUsers={this.suggestedOnclick}/>
-                
+                    <AutoSuggestion suggests={suggest} searchUsers={this.suggestedOnclick}/>        
                 }
-
             </SearchBar>
         )
     }
@@ -115,26 +92,6 @@ class Search extends Component {
 
 export default Search;
 
-
- // ( 
-                //     <div style={{ overflow: 'display', height: '10px', zIndex: '999', width: '210px' }}>
-
-                //        {this.state.suggest.length > 0 && (
-                //         <ul className='ul1-suggest'>
-
-                //             <li>Users</li>
-
-                //         </ul>)}
-
-                //     <ul className='ul2-suggest'>
-
-                //         { 
-                //             suggest.slice(0, 7)
-                //             .map((suggest_result) => <li key={suggest_result.id}
-                //                 onClick={() => { this.suggestedOnclick(suggest_result.login) }} >
-
-                //                 {suggest_result.login}</li>)
-                //         }
 
                 //     </ul>
                 // </div>
